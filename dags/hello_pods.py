517ea6ca287c113b8c4375320c15b5428f917feb
Ubuntu <ubuntu@ip-172-31-40-134.ap-south-1.compute.internal>
2025-08-07 11:18:47 +0000

    Add hello async pod DAG

---
+++ dags/hello_pods.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="hello_async_pods",
+    schedule_interval=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    task = KubernetesPodOperator(
+        task_id="run-async-pod",
+        name="async-hello",
+        namespace="airflow",
+        image="python:3.9",
+        cmds=["python", "-c"],
+        arguments=["print('Hello from a pod!')"],
+        is_delete_operator_pod=True,
+        get_logs=True,
+    )
